e911229d874398076f28c9d48a8ec3b175f8cb63
Andre Moreira <andre.moreira@focus-sys.com.br>
2020-11-18 22:05:09 -0300

    Get lsit from server implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -11,16 +15,24 @@ function App() {
     // TODO
   }
 
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+      console.log(response);
+      console.log('repositories');
+      console.log(repositories);
+    });
+  }, [])
+
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => {
+          return (
+          <li key={repository.id}> {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li> )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
